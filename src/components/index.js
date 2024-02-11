689eb45cdcdbb7a522195e4a35a5c1c7f5c3cb45
BhagyashreeGhodke <drbpghodke@gmail.com>
2024-02-12 00:54:38 +0530

    update

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import Header from "./Header/Header";
+import Home from "./Home/Home";
+import Footer from "./Footer/Footer";
+import About from "./About/About";
+import Contact from "./Contact/Contact";
+import User from "./User/User";
+import GitHub from "./GitHub/GitHub";
+import { githubInfoLoader } from "./GitHub/GitHub";
+
+export {Header,
+        Home,
+        Footer,
+        About, 
+        Contact, 
+        User,
+        GitHub,
+githubInfoLoader}
